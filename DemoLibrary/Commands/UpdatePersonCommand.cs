eace82d6717a1723af49c2fea54fe271eb652760
Israel <realtriplei@gmail.com>
2024-07-14 14:57:27 +0100

    Add project files.

---
+++ DemoLibrary/Commands/UpdatePersonCommand.cs
@@ -0,0 +1,16 @@
+﻿using DemoLibrary.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.Commands
+{
+    public record UpdatePersonCommand(PersonModel data) : IRequest<PersonModel>;
+    
+    //internal class UpdatePersonCommand
+    //{
+    //}
+}
---
+++ DemoLibrary/Handlers/UpdatePersonHandler.cs
@@ -0,0 +1,28 @@
+﻿using DemoLibrary.Commands;
+using DemoLibrary.DataAccess;
+using DemoLibrary.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DemoLibrary.Handlers
+{
+    internal class UpdatePersonHandler : IRequestHandler<UpdatePersonCommand, PersonModel>
+    {
+        private readonly IDataAccess _data;
+        public UpdatePersonHandler(IDataAccess data)
+        {
+            _data = data;
+        }
+
+        public Task<PersonModel> Handle(UpdatePersonCommand request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_data.UpdatePerson(request.data)); 
+        }
+    }
+}
